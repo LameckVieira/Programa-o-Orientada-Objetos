8389103178a4ad3746f654df26b8add81bb8f48f
LameckVieira <lameck.v.barbosa@gmail.com>
2020-12-01 18:54:57 -0300

    Metodos atribuidos

---
+++ Personagem.cs
@@ -0,0 +1,20 @@
+namespace Programa_o_Orientada_Objetos
+{
+    public class personagem
+    {
+       public string nome;
+
+       public int idade;
+
+       public string armadura;
+
+
+       public string Atacar(){
+           return "Ataque de fogo!";
+       } 
+
+       public string Defender(){
+           return "não foi dessa vez!";
+       }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Programa_o_Orientada_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            personagem objetoPersonagem1 = new personagem();
+
+            objetoPersonagem1.nome = "Lameck";
+            objetoPersonagem1.idade = 19;
+            objetoPersonagem1.armadura = "Cabeçada";
+
+
+            personagem objetoPersonagem2 = new personagem();
+
+            objetoPersonagem2.nome = "Paulo";
+            objetoPersonagem2.idade = 30;
+            objetoPersonagem2.armadura = "Flash";
+
+            Console.WriteLine($"Personagem 1 = {objetoPersonagem1.nome} - {objetoPersonagem1.Atacar()}");
+            Console.WriteLine($"Personagem 2 = {objetoPersonagem2.nome} - {objetoPersonagem2.Defender()}");
+        }
+    }
+}
